441c71866d288d5609fceec92b611138f904cb5e
Elise tarres <elise.tarres@studentfr.ch>
2025-01-28 14:24:27 +0100

    formative 0 fini

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,37 @@
 package app;
 
 public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+        public final static int QUANTITE = 1024;
+        public final static int MIN = 1;
+        public final static int MAX = 100;
+
+        public static void main(String[] args) {
+                int[] tab = new int[QUANTITE];
+
+                for (int i = 0; i < tab.length; i++) {
+                        tab[i] = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
+                }
+                int nombrePaire = 0;
+
+                for (int i = 0; i < tab.length; i++) {
+                        if (tab[i] % 2 == 0) {
+                                nombrePaire++;
+                        }
+                }
+                for (int i = 0; i < tab.length; i++) {
+                        System.out.println(tab[i]);
+                }
+                System.out.println("Il y a " + nombrePaire + " nombre pairs dans le tableau");
+                System.out.println("La moyenne des nombres du tableau est " + moyenne(tab));
+        }
+
+        public static double moyenne(int[] tab) {
+                int somme = 0;
+                for (int i = 0; i < tab.length; i++) {
+                        somme += tab[i];
+                }
+                double moyenne = (double) somme / tab.length;
+                return moyenne;
+        }
 
 }
